6a430bee4c2ef4207940762649a6700cc13f37bc
Alberto107 <agil8518>
2026-03-17 14:48:43 -0300

    17_03_2026

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -10,6 +10,26 @@ class Principal extends Controller
         echo 'Página Principal';
     }
 
+    function contato(string $nome){
+        echo $nome;
+    }
+
+    function contatoNomeCompleto(string $nome, string $sobrenome){
+        echo "O contato é $nome $sobrenome";
+    }
+    
+
+    function contatoMensagem(string $nome, string $sobrenome, string $mensagem){
+        echo "O contato é $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+    }
+
+    function contatoTelefone(string $nome, string $sobrenome, string $mensagem, string $telefone, string $email = 'E-mail não informado'){
+        echo "O contato é $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+        echo "Telefone: $telefone";
+        echo "Email: $email"; //-> caso n tenha será mostrado o valor padrão 'E-mail não informado'
+    }
     
 }
 
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,36 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+//:: -> método estático, sem instancia (new) uma classe -> $route = new Route, e na class Route tem static function
 
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('pagina-principal');
 
+//agrupamento de rotas
 
+Route::prefix('publico')->group(function(){
 
+    Route::get('/contato/{nome}', [App\Http\Controllers\Principal::class, 'contato']); //-> recebe um parâmetro
+    Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Principal::class, 'contatoNomeCompleto']); //-> recebe dois parâmetros
+    Route::get('/contato/{nome}/{sobrenome}/{mensagem}', [App\Http\Controllers\Principal::class, 'contatoMensagem']);
+    Route::get('/contato/{nome}/{sobrenome}/{mensagem}?{telefone}/{email?}', [App\Http\Controllers\Principal::class, 'contatoTelefone']); //-> somente o ultimo parametro pode ser opcional
+    
+});
+
+//Pra que serve argupámento de rotas? Para consultas destinadas a um tipo de usuário e consultas para outro tipo
+
+//acesso somente ao admin
+
+// Route::prefix('admin')->group(function(){
+//   Route::get('/painel-financeiro', [App\Http\Controllers\Principal::class, 'contato'])
+//});
+
+//acesso somente ao programador
+
+// Route::prefix('developer')->group(function(){
+//   Route::get('/painel-financeiro', [App\Http\Controllers\Principal::class, 'contato'])
+//});
+
+Route::fallback(function(){ //pesquisar rota e não acha a rota
+    echo "A rota acessada não existe!";
+    echo "<a href='". route('pagina-principal'). "'>Voltar</a>";
+});
